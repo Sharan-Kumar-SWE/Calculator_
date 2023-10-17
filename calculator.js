7404ebc203a32b18bff2cfa4d50815b6aeac9654
sharan <sharankumar282002@gmail.com>
2023-10-17 14:10:35 +0530

    second commit

--- calculator.js
+++ calculator.js
@@ -1,17 +1,23 @@
-let calculation='';
+
+let calculation= JSON.parse(localStorage.getItem('calculated')) || '';
+document.querySelector('.calculate-result').innerHTML=calculation;
+
 
 function updateCalculation(value){
-let result= calculation+=value
-document.querySelector('.calculate-result').innerHTML=result
+    let result= calculation+=value
+    document.querySelector('.calculate-result').innerHTML=result
 }
 
 function isEqual(){
     calculation=eval(calculation)
     document.querySelector('.calculate-result').innerHTML=calculation;
+    localStorage.setItem('calculated',JSON.stringify((calculation)))
 }
+
 
 function clearCalculation(){
     calculation=''
     document.querySelector('.calculate-result').innerHTML=calculation
-   
+    localStorage.removeItem('calculated')
+    
 }
